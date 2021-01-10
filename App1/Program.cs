2605e66e840b2d4f2b0ee89ea32a2de7cdecc7bd
BuketBeker <buketbeker@gmail.com>
2021-01-10 19:06:33 +0300

    ilk gönderme

---
+++ App1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        head:
+            Console.Write("Please enter the square meter of the field:");
+            int field = Convert.ToInt32(Console.ReadLine()); // We take the square meter of the field from the user
+
+            if (field < 0) // The field cannot be negative
+            {
+                Console.WriteLine("The field cannot be negative!!");
+                goto head;
+            }
+
+            Console.Write("Output: ");
+            calculate(field); // We assign the value we get from the user to the calculate method
+
+            Console.ReadKey(); // We use it to keep the output on the screen
+        }
+        static void calculate(int field) // We find the largest possible perfect squares and print them 
+        {
+            if (field > 0)
+            {
+                int i = 0;
+                i = (int)Math.Sqrt(field);
+                field = field - (i * i);
+                Console.Write("{0} ", i * i);
+                calculate(field);
+            }
+        }
+    }
+}
